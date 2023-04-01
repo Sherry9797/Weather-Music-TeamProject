a375950699960957e05036a15953510874049d20
victor.amisi <victor.amisi@dreampayments.com>
2023-04-01 16:09:52 -0400

    I made some changes to the signuo js file

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 //-----------------------------------
 // These code author by Dandan ZHAO
-const apiKey = "c8725c99582e6b7913b5136a66b58338";
+const apiKey = "605c1e9bc5c9408cad4111856223108";
 const city = "Ottawa";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=1&aqi=no&alerts=no`;
 
 // Fetch the weather data from the OpenWeatherMap API
+let weatherValue = document.getElementsByClassName("temp")[0];
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     const temp = Math.round(data.main.temp - 273.15);
     const weather = data.weather[0].main;
     const weatherDescription = data.weather[0].description;
     const weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
     // Display the weather data on the webpage
-    document.getElementById("temp").innerHTML = `${temp}°C`;
+    weatherValue.textContent = `${data.current.temp_c}`;
+    console.log(data);
     document.getElementById(
       "weather"
     ).innerHTML = `${weather} - ${weatherDescription}`;
     document.getElementById("icon").src = weatherIcon;
   })
   .catch((error) => console.log(error));
-
-// When the "Play Music" button is clicked, use the Spotify Web API to play music based on the weather
-const playButton = document.getElementById("play-music");
-playButton.addEventListener("click", () => {
-  const spotifyApiKey = "9a9643ecc0aa417582f0af96a6736da8";
-  const spotifyApiSecret = "6704e1d5f37e40f988186ce24b9f94b4";
-});
 
 // The following code author by Yiqing Song
 // use cookie for log in content
