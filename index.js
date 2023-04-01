508b6281b77b27ec21053b7172e2636c2341f4db
victor.amisi <victor.amisi@dreampayments.com>
2023-04-01 18:11:40 -0400

    added  a few things

--- index.js
+++ index.js
@@ -5,26 +5,6 @@ const apiKey = "605c1e9bc5c9408cad4111856223108";
 const city = "Ottawa";
 const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=1&aqi=no&alerts=no`;
 
-// Fetch the weather data from the OpenWeatherMap API
-let weatherValue = document.getElementsByClassName("temp")[0];
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    const temp = Math.round(data.main.temp - 273.15);
-    const weather = data.weather[0].main;
-    const weatherDescription = data.weather[0].description;
-    const weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-
-    // Display the weather data on the webpage
-    weatherValue.textContent = `${data.current.temp_c}`;
-    console.log(data);
-    document.getElementById(
-      "weather"
-    ).innerHTML = `${weather} - ${weatherDescription}`;
-    document.getElementById("icon").src = weatherIcon;
-  })
-  .catch((error) => console.log(error));
-
 // When the dropdown is changed, fetch and display the weather data for the selected city
 document.getElementById("city").addEventListener("change", () => {
   const city = document.getElementById("city").value;
