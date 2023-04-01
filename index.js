4c7dc36ee6a5013971268748a13d43f1ea95c771
OliverCHO2018 <108848547+OliverCH02018@users.noreply.github.com>
2023-04-01 15:20:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 //-----------------------------------
 // These code author by Dandan ZHAO
-const apiKey = "c8725c99582e6b7913b5136a66b58338";
-const city = "Ottawa";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+const apiKey = 'c8725c99582e6b7913b5136a66b58338'
+const city = document.getElementById('city').value
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
 // Fetch the weather data from the OpenWeatherMap API
 fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const temp = Math.round(data.main.temp - 273.15);
-    const weather = data.weather[0].main;
-    const weatherDescription = data.weather[0].description;
-    const weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  .then(response => response.json())
+  .then(data => {
+    const temp = Math.round(data.main.temp - 273.15)
+    const humidity = data.main.humidity
+    const weather = data.weather[0].main
+    const weatherDescription = data.weather[0].description
+    const weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
 
     // Display the weather data on the webpage
-    document.getElementById("temp").innerHTML = `${temp}°C`;
-    document.getElementById(
-      "weather"
-    ).innerHTML = `${weather} - ${weatherDescription}`;
-    document.getElementById("icon").src = weatherIcon;
+    document.getElementById('temp').innerHTML = `${temp}°C`
+    document.getElementById('humidity').innerHTML = `${humidity}%`
+    document.getElementById('weather').innerHTML = `${weather} - ${weatherDescription}`
+    document.getElementById('icon').src = weatherIcon
   })
-  .catch((error) => console.log(error));
+  .catch(error => console.log(error))
 
-// When the "Play Music" button is clicked, use the Spotify Web API to play music based on the weather
-const playButton = document.getElementById("play-music");
-playButton.addEventListener("click", () => {
-  const spotifyApiKey = "9a9643ecc0aa417582f0af96a6736da8";
-  const spotifyApiSecret = "6704e1d5f37e40f988186ce24b9f94b4";
-});
+// When the dropdown is changed, fetch and display the weather data for the selected city
+document.getElementById('city').addEventListener('change', () => {
+  const city = document.getElementById('city').value
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const temp = Math.round(data.main.temp - 273.15)
+      const humidity = data.main.humidity
+      const weather = data.weather[0].main
+      const weatherDescription = data.weather[0].description
+      const weatherIcon = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+      document.getElementById('temp').innerHTML = `${temp}°C`
+      document.getElementById('humidity').innerHTML = `${humidity}%`
+      document.getElementById('weather').innerHTML = `${weather} - ${weatherDescription}`
+      document.getElementById('icon').src = weatherIcon
+    })
+    .catch(error => console.log(error))
+})
+
+
+
+
+
 
 // The following code author by Yiqing Song
 // use cookie for log in content
