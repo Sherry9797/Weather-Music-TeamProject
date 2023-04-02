69952f60ff4ea7cfb3d82d240f0f29ae59b2fa6c
Sherry <Songyq318@outlook.com>
2023-04-02 17:05:52 -0400

    update

--- login.js
+++ login.js
@@ -46,10 +46,7 @@ function getCookie (cookiename) {
   const $name = cookiename + '='
   const cookieArray = document.cookie.split(';')
   for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i]
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1)
-    }
+    const cookie = cookieArray[i]
     if (cookie.indexOf($name) === 0) {
       return cookie.substring($name.length, cookie.length)
     }
