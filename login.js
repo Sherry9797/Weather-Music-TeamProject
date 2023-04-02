dc3e90a47e3a02849a28a489bcbfe341dead61a5
Sherry <Songyq318@outlook.com>
2023-04-02 16:51:19 -0400

    update

--- login.js
+++ login.js
@@ -1,33 +1,63 @@
 // The following code author by Yiqing Song
 // use cookie for log in content
+
 window.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('login')
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+  const $username = document.getElementById('username')
+  const $password = document.getElementById('password')
+  const $check = document.getElementById('check')
+  const key = 'username'
+  const sevenDays = 7 * 24 * 60 * 60
 
-    const loginUserName = document.getElementById('username')
-    const loginPassword = document.getElementById('password')
-    const checkbox = document.getElementById('check')
-    const key = 'username'
-    const value = encodeURIComponent(username.value)
-    const sevenDays = 7 * 24 * 60 * 60
-
-    const storedPassword = localStorage.getItem(loginUserName.value)
-    if (storedPassword === null) {
-      alert('Username not found')
-    } else if (storedPassword !== loginPassword.value) {
-      alert('Incorrect password')
-    }
-    else if (checkbox.checked) {
-      document.cookie = `${key}=${value};max-age=${sevenDays}`
-      alert(`Thank you for logging in, ${username.value}!`)
-    } else {
-      alert(`Thank you for logging in, ${username.value}!`)
+  // Check if cookie exists
+  const cookieValue = getCookie(key)
+  if (cookieValue) {
+    $username.value = decodeURIComponent(cookieValue)
+    const storedPassword = localStorage.getItem(cookieValue)
+    if (storedPassword) {
+      $password.value = storedPassword
     }
   }
-  )
+
+  // check username and password
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const enteredUsername = $username.value
+    const enteredPassword = $password.value
+    const storedPassword = localStorage.getItem(enteredUsername)
+    if (storedPassword === null) {
+      alert('Username not found')
+    } else if (storedPassword !== enteredPassword) {
+      alert('Incorrect password')
+    } else {
+      if ($check.checked) {
+        document.cookie = `${key}=${encodeURIComponent(enteredUsername)};max-age=${sevenDays}`
+      } else {
+        if (getCookie(key)) {
+          document.cookie = `${key}=;expires=Thu, 01 Jan 1970 00:00:01 GMT`
+        }
+      }
+      alert(`Thank you for logging in, ${enteredUsername}!`)
+    }
+  })
+})
+// get cookie value
+function getCookie (cookiename) {
+  const $name = cookiename + '='
+  const cookieArray = document.cookie.split(';')
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i]
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1)
+    }
+    if (cookie.indexOf($name) === 0) {
+      return cookie.substring($name.length, cookie.length)
+    }
+  }
+  return null
 }
-)
+
+// open in a popup window
 const $modal = document.getElementById('modal')
 
 const windowFeatures = 'height=500,width=500,top=' + (screen.height / 2 - 250) + ',left=' + (screen.width / 2 - 250)
